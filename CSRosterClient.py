714df4b0a6d4599a81adfe88d46f253bff2415f0
YungFell0ut <32377119+YungFell0ut@users.noreply.github.com>
2020-05-09 02:44:22 -0700

    Update CSRosterClient.py

--- CSRosterClient.py
+++ CSRosterClient.py
@@ -1,7 +1,7 @@
 import socket
 
 ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ClientSocket.connect(("192.168.56.1", 1337))
+ClientSocket.connect(("IP_ADDRESS", 1337))
 
 while True:
     msg = ClientSocket.recv(4096)
